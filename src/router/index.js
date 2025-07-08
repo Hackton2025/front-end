46931729b5e59ca02aad7a2d0763bc4af324fc01
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-07-08 16:52:21 -0300

    FEAT: Criei a view e o component da homePage, e arrumei a questão das rotas #3

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import loginView from '@/views/loginView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'login',
       component: loginView,
     },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+    }
 
   ],
 })
