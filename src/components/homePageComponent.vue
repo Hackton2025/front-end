0484d5c96315185a172e8fa44903cb681048a4e5
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-09-03 14:57:20 -0300

    FEAT: Merge da feat 51 na 44 FEAT: Criar página para mandar mensagens massivas Fixes #44

--- src/components/homePageComponent.vue
+++ src/components/homePageComponent.vue
@@ -5,17 +5,49 @@ import headerComponent from './headerComponent.vue'
 import instituicaoComponent from './instituicaoComponent.vue'
 import comunidadesComponent from './comunidadesComponent.vue'
 import configPanelComponent from './configPanelComponent.vue'
+import storysComponent from './storysComponent.vue'
+import homeContentComponent from './homeContentComponent.vue'
+
 
 const mostrarConfig = ref(false)
 const isOn = ref(false)
 </script>
 
 <template>
-    <header-component />
-    <instituicao-component />
-    <comunidades-component />
+     <header-component />
+    <main>
+        <div class="sidebar-left">
+            <instituicao-component />
+            <comunidades-component />
+        </div>
+        <div class="center">
+            <storys-component />
+            <home-content-component />
+        </div>
+     
+    </main>
 </template>
 <style scoped>
+main {
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+    grid-template-areas: 
+        'left center right';
+    width: 100vw;
+}
+
+.sidebar-left {
+    grid-area: left;
+}
+
+.sidebar-right {
+    grid-area: right;
+}
+.center {
+    grid-area: center;
+}
+
+
 
 </style>
 
