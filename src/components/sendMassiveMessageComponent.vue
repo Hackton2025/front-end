be7795b558cf0221d5dab55b6a43d9cb6629c12a
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-09-05 14:57:46 -0300

    FEAT: continuação FEAT: Criar página para mandar mensagens massivas Fixes #44

--- src/components/sendMassiveMessageComponent.vue
+++ src/components/sendMassiveMessageComponent.vue
@@ -1,5 +1,33 @@
 <script setup>
-    import { ref, reactive } from 'vue';
+    import { ref, reactive } from 'vue'; 
+    import api from "@/plugins/axios";
+
+    async function getImageUploader(attachment_key) {
+        const uploader_image = {
+        url: "",
+        attachment_key: attachment_key,
+        uploaded_on: "",
+        }
+        
+
+        const response = await api.get("/image-uploader/", image-uploader)
+        return response.data
+    }
+
+    const content = {
+        author: "3cf0b59b-1024-42a2-a55f-b741f64d7bb2", 
+        content: "porra", 
+        image: getImageUploader("0999ff91-c365-4615-b099-6f692223297b"), 
+        video: null,
+        emergency: true,
+    }
+
+    async function createMassiveMessage() {
+       const response = await api.post("/content/", content)
+       return response.data    
+    }
+
+    console.log(createMassiveMessage())
 
 
 </script>
@@ -11,8 +39,11 @@
         </section>
 
         <section class="selecionar-recebedores">
-            
+            <div class="conteiner-checkbox">
+                
+            </div>
         </section>
+        <section class="right-massive"></section>
     </main>
 </template>
 <style scoped>
