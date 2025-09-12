81a987d573a0c503b8442379c04848e28c9158ee
LauraEloysa <lauraeloysad@gmail.com>
2025-09-12 15:04:34 -0300

    FEAT: Exibição de comunidade #36

--- src/components/comunidadesComponent.vue
+++ src/components/comunidadesComponent.vue
@@ -1,50 +1,50 @@
 <script setup>
+import { useComunittyStore } from '@/stores/comunitty';
+const store = useComunittyStore()
 </script>
 
 <template>
-      <div class="comunidades">
-        <h3>Minhas Comunidades:</h3>
-        <ul>
+  <div class="comunidades">
+    <h3>Minhas Comunidades:</h3>
+    <ul>
 
-          <li><img src="/public/img/direcao-g.png" alt="D-geral"> <p>Direção-Geral</p> </li>
+      <li v-for="value in store.filteredCommunities" :key="value.uuid">
+        <img :src="value.image" alt="Imagem comunidade">
+        <p>{{ value.name }}</p>
+      </li>
 
-          <li> <img src="/public/img/cecom.png" alt="cecom"> <p>CECOM</p> </li>
 
-          <li> <img src="/public/img/agropecuaria.png" alt="agro"> <p>Agropecuária</p> </li>
-
-          <li> <img src="/public/img/informatica.png" alt="info"> <p>Informática</p> </li>
-
-          <li> <img src="/public/img/quimica.png" alt="quimi"> <p>Quimica</p> </li>
-
-          <li> <img src="/public/img/nupe.png" alt="nupe"> <p>NUPE</p> </li>
-
-        </ul>
-      </div>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-.comunidades{
+.comunidades {
   margin: 2vw 0 0 6vw;
 }
+
 h3 {
   font-size: 1.5rem;
   font-weight: bolder;
   margin-bottom: 1.5vw;
 }
+
 li {
   display: flex;
   padding: 10px;
 }
+
 p {
   display: flex;
   align-items: center;
   margin: 10px;
 }
+
 img {
   width: 3vw;
   height: 3vw;
   border-radius: 15%;
   padding: 4.5px;
   background-color: #2F9E41;
- }
+}
 </style>
