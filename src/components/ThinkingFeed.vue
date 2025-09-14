debb345734f276845e741cdfa11b627d05c8d67d
user.name: kauabarros-25 <user.mail: martinsbarroskaua@gmail.com>
2025-09-13 22:30:25 -0300

    CHORE: Create input

--- src/components/ThinkingFeed.vue
+++ src/components/ThinkingFeed.vue
@@ -1,14 +1,54 @@
 <template>
-    <div class="main"></div>
-    
+  <div class="main">
+    <div class="profile"></div>
+    <div class="input-wrapper">
+      <input type="text" class="input" placeholder="O que você está pensando?">
+    </div>
+  </div>
 </template>
+
 <style scoped>
-.main{
-    height: 80px;
-    border: 1px solid black;
-    background-color: lightgreen;
-    border-radius: 10px;
-    margin-bottom: 20px;
-    width: 95%;
+.main {
+  height: 80px;
+  background-color: #FFFFFF;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  width: 95%;
+  display: grid;
+  grid-template-columns: 1fr 9fr; 
+  align-items: center; 
+  gap: 10px;
+  padding: 0 15px; 
+  box-sizing: border-box;
+}
+
+.profile {
+  border: solid 1px black;
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+}
+
+.input-wrapper {
+  width: 100%;
+  height: 50px;
+  background-color: #F0F2F5;
+  border-radius: 10px;
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+}
+
+.input {
+  border: none;
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  font-size: 14px;
+}
+
+.input:focus {
+  outline: none;
 }
 </style>
