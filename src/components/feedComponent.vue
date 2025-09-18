c49b37afa6aa01839656ab1bcac88f4682d3c2b7
vitormanoelssantana <vitormanoels.santana@gmail.com>
2025-09-18 13:29:20 -0300

    FEAT:

--- src/components/feedComponent.vue
+++ src/components/feedComponent.vue
@@ -5,43 +5,55 @@ const store = usePostStore();
 </script>
 
 <template>
-  <ul>
-    <li v-for="post in store.posts" :key="post.uuid">
-      <div class="post">
-      <div class="total">
-        <div class="author">
-          <img
-            v-if="post.author.first_profile_image_url"
-            :src="post.author.first_profile_image_url"
-            alt="foto de perfil"
-          />
-          <p>{{ post.author.fullname }}</p>
-        </div>
-        <div class="content">
-          <p>{{ post.content }}</p>
+  <main>
+    <ul>
+      <li v-for="post in store.posts" :key="post.uuid">
+        <div class="post">
+          <div class="total">
+            <div class="author">
+              <img v-if="post.author.first_profile_image_url" :src="post.author.first_profile_image_url"
+                alt="foto de perfil" />
+              <p>{{ post.author.fullname }}</p>
+            </div>
+            <div class="content">
+              <p>{{ post.content }}</p>
 
-          <img
-            v-if="post.image_url"
-            :src="post.image_url"
-            alt="imagem do post"
-          />
+              <img v-if="post.image_url" :src="post.image_url" alt="imagem do post" />
 
-          <video v-if="post.video_url" :src="post.video_url" controls></video>
+              <video v-if="post.video_url" :src="post.video_url" controls></video>
+            </div>
+          </div>
         </div>
-        </div>
+      </li>
+    </ul>
+
+    <div style="background-color: white; padding: 2vw 3vw; border-radius: 10px; height: fit-content;">
+      <h2 style="font-size: 2rem;" class="instituicoes">
+        Agora no IFC
+      </h2>
+      <div class="teste" style="display: flex; align-items: center; margin-top: 2vw;">
+        <img style="width: 4vw;" src="/public/img/Logo_IFC_vertical.png" alt="">
+        <h2>IFC Campus Araquari</h2>
       </div>
-    </li>
-  </ul>
+    </div>
+  </main>
 </template>
 <style scoped>
-ul{
+main{
+  display: flex;
+  justify-content: space-between;
+  margin: 1vw 4vw;
+}
+ul {
   justify-content: center;
 }
+
 .post {
   width: 60%;
-  min-width:60%;
+  min-width: 60%;
 }
-.total{
+
+.total {
   background-color: white;
   margin-bottom: 2vw;
   width: 100%;
@@ -54,6 +66,7 @@ ul{
   justify-content: left;
   align-items: center;
 }
+
 .author img {
   max-width: 10%;
   min-width: 10%;
@@ -61,6 +74,7 @@ ul{
   max-height: 8vh;
   border-radius: 100%;
 }
+
 .author p {
   font-size: 1.5rem;
   margin-left: 1vw;
@@ -75,9 +89,9 @@ ul{
 
 .content p {
   font-size: 1rem;
-  white-space: pre-wrap; 
-  word-wrap: break-word; 
+  white-space: pre-wrap;
+  word-wrap: break-word;
   overflow-wrap: anywhere;
-  margin: 20px 0; 
+  margin: 20px 0;
 }
 </style>
